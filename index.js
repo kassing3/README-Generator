b9a9ed225eca8299e86eca8ff6cd74bef0e06f25
kassing3 <108312371+kassing3@users.noreply.github.com>
2022-11-01 20:21:16 -0400

    Version 1 of Application

---
+++ index.js
@@ -0,0 +1,79 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message:"Please provide a description of your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message:"Installation Instructions: What are the steps required to install your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message:"Usage Information: Provide instructions on how to use your program and example of use",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message:"Contributions: Please credit any additional contributors, resources, or collaborators.",
+        name: "contributions",
+    },
+    {
+        type: "input",
+        message:"Testing Instructions: Provide written out tests for you application with provided examples on how to run them.",
+        name: "tests",
+    },
+    {
+        type: "list",
+        message:"Please select the license that applies to your application.",
+        name: "license",
+        choices: ["Apache 2.0 License", "Boost Software License 1.0", "BSD 3-Clause License", "Creative Commons", "Eclipse Public License 1.0", "GNU GPL v3", "The Hippocratic License 3.0", "IBM Public License Version 1.0", "ISC License (ISC)", "The MIT License", "Mozilla Public License 2.0", "Open Data Commons", "The Perl License", "SIL Open Font License 1.1", "Unlicense"],
+    },
+    {
+        type: "input",
+        message:"What's your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message:"What's your email",
+        name: "email",
+    },
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+   return writeFile(fileName, generateMarkdown(data));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+   inquirer
+   .prompt(questions)
+   .then((answers) => 
+     writeToFile('README.md', answers)
+    )
+   .then (() =>
+        console.log('Success!')
+    ) 
+    .catch( (err) =>
+        console.log(err)
+    )
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,125 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      return "https://img.shields.io/badge/License-Apache_2.0-blue.svg"
+    case "Boost Software License 1.0":
+      return "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg"
+    case "BSD 3-Clause License":
+      return "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"
+    case "Creative Commons":
+      return "https://licensebuttons.net/l/zero/1.0/80x15.png"
+    case "Eclipse Public License 1.0":
+      return "https://img.shields.io/badge/License-EPL_1.0-red.svg"
+    case "GNU GPL v3":
+      return "https://img.shields.io/badge/License-GPLv3-blue.svg"
+    case "The Hippocratic License 3.0":
+      return "https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg"
+    case "IBM Public License Version 1.0":
+      return "https://img.shields.io/badge/License-IPL_1.0-blue.svg"
+    case "ISC License (ISC)":
+      return "https://img.shields.io/badge/License-ISC-blue.svg"
+    case "The MIT License":
+      return "https://img.shields.io/badge/License-MIT-yellow.svg"
+    case "Mozilla Public License 2.0":
+      return "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg"
+    case "Open Data Commons":
+      return "https://img.shields.io/badge/License-ODC_BY-brightgreen.svg"
+    case "The Perl License":
+      return "https://img.shields.io/badge/License-Perl-0298c3.svg"
+    case "SIL Open Font License 1.1":
+      return "https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg"
+    case "Unlicense":
+      return "https://img.shields.io/badge/license-Unlicense-blue.svg"
+    default:
+      return ""
+  }
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      return "https://opensource.org/licenses/Apache-2.0"
+    case "Boost Software License 1.0":
+      return "https://www.boost.org/LICENSE_1_0.txt"
+    case "BSD 3-Clause License":
+      return "https://opensource.org/licenses/BSD-3-Clause"
+    case "Creative Commons":
+      return "http://creativecommons.org/publicdomain/zero/1.0/"
+    case "Eclipse Public License 1.0":
+      return "https://opensource.org/licenses/EPL-1.0"
+    case "GNU GPL v3":
+      return "https://www.gnu.org/licenses/gpl-3.0"
+    case "The Hippocratic License 3.0":
+      return "https://firstdonoharm.dev"
+    case "IBM Public License Version 1.0":
+      return "https://opensource.org/licenses/IPL-1.0"
+    case "ISC License (ISC)":
+      return "https://opensource.org/licenses/ISC"
+    case "The MIT License":
+      return "https://opensource.org/licenses/MIT"
+    case "Mozilla Public License 2.0":
+      return "https://opensource.org/licenses/MPL-2.0"
+    case "Open Data Commons":
+      return "https://opendatacommons.org/licenses/by/"
+    case "The Perl License":
+      return "https://opensource.org/licenses/Artistic-2.0"
+    case "SIL Open Font License 1.1":
+      return "https://opensource.org/licenses/OFL-1.1"
+    case "Unlicense":
+      return "http://unlicense.org/"
+    default:
+      return ""
+  }
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ![${data.license}](${renderLicenseBadge(data.license)})(${renderLicenseLink(data.license)})
+
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under following license: [${data.license}](${renderLicenseLink(data.license)})
+
+  ## Contributing 
+  ${data.contributions}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+
+  If you have any questions about this repository or to find more of my work, please open an issue or view my [GitHub](https://github.com/${data.username}). You're more than welcome to also contact me directly via email at ${data.email}.
+
+`;
+}
+
+module.exports = generateMarkdown;
